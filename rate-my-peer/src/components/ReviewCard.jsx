9e4b24b7f4b7a7a36b6ea50d8796c52286ae593b
Lkleppe <kleppeluke05@gmail.com>
2026-05-08 09:51:42 -0700

    ReviewForm now shows if the reviewer would work with the reviewee again

--- rate-my-peer/src/components/ReviewCard.jsx
+++ rate-my-peer/src/components/ReviewCard.jsx
@@ -67,7 +67,8 @@ export default function ReviewCard({ review, currentUserId }) {
                 <span>{review.course} - {review.instructor} - {review.project}</span>
                 <span>{review.rating}.0 / 5</span>
             </header>
-            <p>{review.comment}</p>
+            <p className='review-comment'>{review.comment}</p>
+            <p><i>Would work with again:</i><strong>{review.wouldWorkWithAgain !== undefined ? (review.wouldWorkWithAgain ? "Yes" : "No") : null}</strong></p>
             <div className='review-card-footer'>
                 <div className="tag-row">
                 <span className='review-date'>{createdAt}</span>
