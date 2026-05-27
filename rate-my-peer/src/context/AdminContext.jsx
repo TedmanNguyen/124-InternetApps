432071453006483b5ff9dfe8f5c44fd828c0d907
Lkleppe <kleppeluke05@gmail.com>
2026-05-26 20:00:20 -0700

    fixed AdminContext location

---
+++ rate-my-peer/src/context/AdminContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useMemo, useState, useEffect } from "react";
+import axios from "axios";
+import { urls } from '../data/urls';
+
+const AdminContext = createContext(null)
+
+export const AdminProvider = ({ children }) => {
+    const [reports, setReports] = useState([])
+
+    useEffect(() => {
+        const fetchReports = async () => {
+            try {
+                const response = await axios.get(`${urls.base}/${urls.reports}`)
+                setReports(response.data)
+            } catch (error) {
+                console.error('Error fetching reports:', error)
+            }
+        }
+
+        fetchReports()
+    }, [])
+
+    const setReportStatus = ( {reportId, status} ) => {
+        setReports(prevReports => 
+            prevReports.map(report => 
+                report._id === reportId ? { ...report, status } : report
+            )
+        )
+    }
+
+    const value = useMemo(() => ({
+        reports,
+        setReportStatus,
+    }), [reports])
+
+    return (
+        <AdminContext.Provider value={value}>
+            {children}
+        </AdminContext.Provider>
+    )
+}
+
+export const useAdmin = () => {
+    const context = useContext(AdminContext)
+    if (!context) {
+        throw new Error("useAdmin must be used within an AdminProvider")
+    }
+    return context
+}
